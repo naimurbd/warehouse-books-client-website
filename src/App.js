7761a21e8d6f81d572f36fd95237476dcf75068a
naimurbd <naiemnaiem072@gmail.com>
2022-04-30 15:22:37 +0600

    Implement email/password-based authentication

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Checkout from './Pages/Checkout/Checkout/Checkout';
 import Home from './Pages/Home/Home/Home';
 import ItemDetail from './Pages/ItemDetail/ItemDetail';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
+import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import NotFound from './Pages/Shared/NotFound/NotFound';
@@ -20,6 +22,11 @@ function App() {
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/checkout" element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+        }></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/ItemDetail/ItemDetail.js
+++ src/Pages/ItemDetail/ItemDetail.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const ItemDetail = () => {
     const { inventoryId } = useParams();
     return (
         <div>
             <h2>welcome to detail: {inventoryId}</h2>
+            <div className='text-center'>
+                <Link to="/checkout">
+                    <button className='btn btn-primary'>Proceed Checkout</button>
+                </Link>
+            </div>
         </div>
     );
 };
